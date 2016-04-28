5bf3641937c875207ff6f4a4a9ac36e188c20a06
Katia-Lamargot <katialamargot@gmail.com>
2016-04-28 17:09:19 +0200

    50 fichiers lorem

---
+++ lorem.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'inc/config.php';
+
+$contents = nl2br(file_get_contents('lorem.txt'));
+$contents = explode('<br />'.PHP_EOL.'<br />'.PHP_EOL, $contents);
+
+debug($contents);
+
+foreach ($contents as $key => $content) {
+	file_put_contents("Lorem$key.txt", $content);
+}
